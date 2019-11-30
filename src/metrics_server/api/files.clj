4cc4e3b6f07db50b6e55610324ae0941d9f612a2
absdspr <kiriys36@gmail.com>
2019-11-30 12:11:11 +0300

    task2 completed

--- src/metrics_server/api/files.clj
+++ src/metrics_server/api/files.clj
@@ -18,3 +18,42 @@
   "Get files in directory on server"
   []
   (:data (get-files-with-http-info)))
+
+;; Метод убирает все директории
+(defn task21 [files]
+  (filter (fn [x] (false? (x :directory))) files)
+  )
+
+;; Метод убирает все неисполняемые файлы
+(defn task22 [files]
+  (filter (fn [x] (true? (x :executable))) files))
+
+
+;; Метод изменяет расширение файла с .conf на .cfg
+(defn changeExtension [filename]
+  (clojure.string/replace filename #".conf" ".cfg" ))
+
+;; Метод изменяет расширения файлов с .conf на .cfg
+(defn task23 [files]
+  (map (fn [file]
+         {
+          :name (changeExtension (file :name))
+          :size (file :size)
+          :changed (file :changed)
+          :directory (file :directory)
+          :executable (file :executable)
+          })))
+
+;; Метод считает средний размер файла не директории
+(defn task24 [files]
+  (/
+    (reduce + (map (fn [file] (file :size)) (filter (fn [x] (false?(x :directory))) files)))
+    (count (filter (fn [x] (false?(x :directory))) files))
+    )
+  )
+
+(defn -main []
+  (println (task21 (get-files)))
+  (println (task22 (get-files)))
+  (println (task23 (get-files)))
+  (println (task24 (get-files))))
