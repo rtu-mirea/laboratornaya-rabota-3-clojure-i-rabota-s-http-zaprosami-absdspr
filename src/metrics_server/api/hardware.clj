1a9cd08321fe2d1e4eadef0504690d2194911d02
absdspr <kiriys36@gmail.com>
2019-11-30 11:34:49 +0300

    Task 1

---
+++ src/metrics_server/api/hardware.clj
@@ -0,0 +1,48 @@
+(ns metrics-server.api.hardware
+  (:require [metrics-server.core :refer [call-api check-required-params with-collection-format]])
+  (:import (java.io File)))
+
+(defn get-metrics-with-http-info
+  "Get hardware metrics"
+  []
+  (call-api "/hardware" :get
+            {:path-params   {}
+             :header-params {}
+             :query-params  {}
+             :form-params   {}
+             :content-types []
+             :accepts       []
+             :auth-names    []}))
+
+(defn get-metrics
+  "Get hardware metrics"
+  []
+  (:data (get-metrics-with-http-info)))
+
+
+;; Метод суммирует по полю cpuTemp
+(defn sumCpuTemp [hardwareMetrics]
+  (reduce + (map (fn [x] (x :cpuTemp)) hardwareMetrics)))
+
+;; Метод суммирует по полю cpuLoad
+(defn sumCpuLoad [hardwareMetrics]
+  (reduce + (map (fn [x] (x :cpuLoad)) hardwareMetrics)))
+
+(defn average [sum count]
+  (/ (sum count)))
+
+
+(defn task11 [hardwareMetrics]
+  (filter (fn [x] (> (x :cpuTemp) 2)) hardwareMetrics)
+  )
+
+(defn task12 [hardwareMetrics]
+  (/ (sumCpuTemp hardwareMetrics) (count hardwareMetrics)))
+
+(defn task13 [hardwareMetrics]
+  (/ (sumCpuLoad hardwareMetrics) (count hardwareMetrics)))
+
+(defn -main []
+  (println (task11 (get-metrics)))
+  (println (task12 (get-metrics)))
+  (println (task13 (get-metrics))))
